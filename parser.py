7c287ea3f9848e3ec47905b61bf90a5f35f9e73c
sindhu alagesan <sindhu.ceg@gmail.com>
2013-04-17 00:57:59 -0500

    updated parse

--- parser.py
+++ parser.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python -tt
 import bs4
 import sys
+import os
+import json
 
 #this file contains the code snippet to parse HTML
 def main():
-	f=open("/Users/Macho/Developer/IR/sample2.html")
-	html=f.read()
-	soup=bs4.BeautifulSoup(html)
-	print soup.body.find('span',attrs={'class':'entry'}).text
+	
+	dict1 = {}
+	filelist = os.listdir("C:\Users\sindhu\Desktop\ir\project\careercup")
+	print len(filelist)
+    i = 1
+    """   
+   	for file in filelist:  
+   	    dict2 = {}
+		f=open(file)
+		html=f.read()
+		soup=bs4.BeautifulSoup(html)
+		qn = soup.body.find('span',attrs={'class':'entry'}).find('p').text
+		print qn
+		dict2['qn'] = qn
+		tmp = soup.body.find('span',attrs={'class':'tags'}).find_all('a')
+		company = tmp[0].contents[0]
+		position = tmp[1].contents[0]
+		type =  tmp[2].contents[0]
+	    dict2['company'] = company
+	    dict2['position'] = position
+	    dict2['type'] = type
+		print company , position , type
+		dict1[i] = dict2
+		i += 1
+	"""
+	with open('data.txt', 'w') as outfile:
+    json.dump(dict1, outfile)
 	sys.exit(0)
 				
 if __name__ == '__main__':
