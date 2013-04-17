4aa1a2c972efede59d00290eed255deb5fd2f25b
802dreamer <vasuec@gmail.com>
2013-04-16 23:35:48 -0500

    commiting

--- parser.py
+++ parser.py
@@ -2,12 +2,13 @@
 import bs4
 import sys
 
+#this file 
 #this file contains the code snippet to parse HTML
 def main():
 	f=open("/Users/Macho/Developer/IR/sample2.html")
 	html=f.read()
 	soup=bs4.BeautifulSoup(html)
-	print soup.body.find('span',attrs={'class':'entry'}).text
+	print soup.body.find('span',attrs={'class':'entry'}).find('p').text
 	sys.exit(0)
 				
 if __name__ == '__main__':
